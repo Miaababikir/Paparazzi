72397346d7b57488b29500a8dafd19b91329343a
Mosab Ibrahim <miaababikir@gmail.com>
2020-04-20 22:11:40 +0200

    Setting purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,22 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.vue',
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
+        ...mix.inProduction() ? [purgecss] : [],
         require('autoprefixer'),
     ]);
 
